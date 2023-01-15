5fb67e9b374c06747fa0b6ae57792153b4fcaff0
Ilyacnt <ilyagt2012@gmail.com>
2023-01-15 23:39:04 +0300

    #2 get next port func

--- server/index.js
+++ server/index.js
@@ -1,17 +1,19 @@
 // Файл должен запускать сервер на основе билда в /dist
 const express = require('express')
+const getNextPort = require('./getNextPort')
 const path = require('path')
 const app = express()
 
 app.use(express.static(path.join(__dirname, '..', "dist")))
 
-const PORT = 3000
 
 const start = () => {
     try {
-        app.listen(PORT, () => {
-            console.log(`Server listening on port ${PORT}`);
-        })
+        getNextPort(3000).then(port => {
+            const server = app.listen(port, () => {
+                console.log(`Server listening on http://localhost:${server.address().port}`);
+            })
+        });
     } catch (error) {
         console.log(error)
     }
