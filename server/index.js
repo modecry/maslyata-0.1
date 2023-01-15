fedac03b378b2c078280fc68231695965bde2c94
Ilyacnt <ilyagt2012@gmail.com>
2023-01-15 19:05:41 +0300

    #2 server and build script

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
 // Файл должен запускать сервер на основе билда в /dist
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static(path.join(__dirname, '..', "dist")))
+
+const PORT = 3000
+
+const start = () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server listening on port ${PORT}`);
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
