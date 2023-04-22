3572b4cd0bc25813019183f058edb3f7bf4cebdd
Ilyacnt <ilyagt2012@gmail.com>
2023-04-22 19:45:30 +0300

    production config and style auto link

---
+++ config/webpack.prod.js
@@ -0,0 +1,18 @@
+const webpackCommonConfig = require('./webpack.common.js');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+
+config = {
+    ...webpackCommonConfig,
+    optimization: {
+        ...webpackCommonConfig.optimization,
+        minimizer: [
+            new CssMinimizerPlugin()
+        ]
+    },
+    mode: 'production',
+}
+
+delete config.devtool
+
+module.exports = config
