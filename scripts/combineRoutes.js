6f18f5eeb3a9c8faa6fed1fcce08fcd7ac82bd39
Ilyacnt <ilyagt2012@gmail.com>
2023-04-23 13:40:40 +0300

    combineroutes move to scripts folder

--- scripts/combineRoutes.js
+++ scripts/combineRoutes.js
@@ -1,1 +1,19 @@
-// export const combineRoutes = () => {} ===>return ["home", "blog", ....] as json file in ./config/routes.json
+const fs = require('fs')
+const path = require('path')
+const paths = require('../config/paths')
+
+const getPageFolders = (path) => {
+  return fs
+    .readdirSync(path, { withFileTypes: true })
+    .filter((folder) => folder.isDirectory())
+    .map((folder) => folder.name)
+}
+
+const writeJSON = (pages) => {
+  return fs.writeFileSync(
+    path.resolve(__dirname, '..', 'config', 'pages.json'),
+    JSON.stringify({ pages })
+  )
+}
+
+writeJSON(getPageFolders(paths.pages))
